132d76ec99eac9d1a56bfd1d0e3066217492e684
thomaszampa <thomas.zampa@gmail.com>
2019-02-20 12:50:10 -0600

    Changed boss animator and second stage actions

--- Assets/Scripts/Whitch.cs
+++ Assets/Scripts/Whitch.cs
@@ -1,17 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Whitch : MonoBehaviour
 {
 
-    public float health;
     public float damage;
+    public float startHealth = 100;
+
+    [HideInInspector]
+
+    private float health;
+
+    public GameObject deathEffect;
+
+    [Header("Unity Stuff")]
+
+    public Image healthBar;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        health = startHealth;
+    }
+
+    public void TakeDamage (float amount)
+    {
+        health -= amount;
+
+        healthBar.fillAmount = health / startHealth;
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 
     // Update is called once per frame
